ab5ebf694258c9916c7f35ec9690ac739b846cba
Ebi <ebi.banayan@gmail.com>
2024-03-03 18:06:18 +1300

    Update gilded_rose.js

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -5,9 +5,10 @@ class Item {
     this.quality = quality;
   }
 
-  updateQuality() {
-    if (this.quality < 50) {
-      this.quality = this.quality + 1;
+  updateQuality(by = 1, maxRange = 50) {
+    const isBetween = (value) => value > 0 && value < maxRange
+    if (isBetween(this.quality)) {
+      this.quality = this.quality + by;
     }
   }
 
@@ -15,24 +16,21 @@ class Item {
     if (this.name == 'Aged Brie') {
       this.updateQuality();
     } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1;
-        if (this.sellIn < 11) {
-          this.updateQuality();
-        }
-        if (this.sellIn < 6) {
-          this.updateQuality();
-        }
+      this.updateQuality()
+      if (this.sellIn < 11) {
+        this.updateQuality();
+      }
+      if (this.sellIn < 6) {
+        this.updateQuality();
       }
     } else if (this.name == 'Sulfuras, Hand of Ragnaros') {
-      this.quality -= 0;
+      this.updateQuality(0, 80);
     } else {
-      if (this.quality > 0) {
-        this.quality -= 1;
-      }
+      this.updateQuality(-1);
     }
 
     if (this.name == 'Sulfuras, Hand of Ragnaros') {
+      this.updateQuality(0, 80);
     } else {
       this.sellIn = this.sellIn - 1;
     }
@@ -42,9 +40,7 @@ class Item {
       } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
         this.quality = 0;
       } else if (this.name == 'Sulfuras, Hand of Ragnaros') {
-        if (this.quality > 0) {
-          this.quality = this.quality - 1;
-        }
+        this.updateQuality(-1, 80);
       }
     }
   }
